31e2a807ac638fe198e0fdb8de95321bafe6512c
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-01-29 03:58:33 +0800

    removed admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,11 @@ import { Role } from './_models';
 
 const homeModule = () => import('./pages/home/home.module').then(x => x.HomeModule);
 const accountModule = () => import('./pages/account/account.module').then(x => x.AccountModule);
-const adminModule = () => import('./pages/admin/admin.module').then(x => x.AdminModule);
 const userModule = () => import('./pages/user/user.module').then(x => x.UserModule);
 
 const routes: Routes = [
     { path: '', loadChildren: homeModule},
     { path: '', loadChildren: accountModule },
-    { path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
     { path: 'user', loadChildren: userModule, canActivate: [AuthGuard], data: { roles: [Role.User] } },
 
     // otherwise redirect to home
