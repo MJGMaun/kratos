966b2380425f1a6be928442f3458ef4c0476a637
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-01-29 03:21:29 +0800

    removed profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,11 @@ import { Role } from './_models';
 const homeModule = () => import('./pages/home/home.module').then(x => x.HomeModule);
 const accountModule = () => import('./pages/account/account.module').then(x => x.AccountModule);
 const adminModule = () => import('./pages/admin/admin.module').then(x => x.AdminModule);
-const profileModule = () => import('./pages/profile/profile.module').then(x => x.ProfileModule);
 const userModule = () => import('./pages/user/user.module').then(x => x.UserModule);
 
 const routes: Routes = [
     { path: '', loadChildren: homeModule},
     { path: '', loadChildren: accountModule },
-    { path: 'profile', loadChildren: profileModule, canActivate: [AuthGuard] },
     { path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard], data: { roles: [Role.Admin] } },
     { path: 'user', loadChildren: userModule, canActivate: [AuthGuard], data: { roles: [Role.User] } },
 
