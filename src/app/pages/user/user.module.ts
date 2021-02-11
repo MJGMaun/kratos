7ff02997c546a7553d72957d89b4d7c0f13b772d
MJ Maun <Mikhaela.Maun@MytikasLLC.com>
2021-02-12 04:19:45 +0800

    added form and material modules

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from '../../shared/material/material.module';
 
 import { UserRoutingModule } from './user-routing.module';
 import { LayoutComponent } from './layout.component';
@@ -10,7 +13,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [LayoutComponent, DashboardComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class UserModule { }
